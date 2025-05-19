8c78c775d35ff60695b951549aa9f4b69ee0c206
maksymkulbaka <mrskylineer@gmail.com>
2025-05-19 16:19:32 +0300

    feat: add class Category
    The Category class represents product categories in a store system using a name and a list of products.
    The class uses the same features as the Product class.

--- store-management.py
+++ store-management.py
@@ -26,5 +26,30 @@ class Product:
             raise TypeError("Argument should be str data type")
         self._name = new
 
+class Category:
+    def __init__(self, name: str, products: list):
+        if isinstance(name, str):
+            raise TypeError("Argument 'name' should be str data type")
+        if isinstance(products, list):
+            raise TypeError("Argument 'products' should be list data type")
+        self._name = name
+        self._products = products
+
+    def get_info(self) -> dict:
+        return {'name': self._name, 'products': self._products}
+
+    def __str__(self):
+        str(self.get_info())
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, new):
+        if not isinstance(new, str):
+            raise TypeError("Argument should be str data type")
+        self._name = new
+
 if __name__ == "__main__":
     prod1 = Product("Sausage", "Meat", 3050)
